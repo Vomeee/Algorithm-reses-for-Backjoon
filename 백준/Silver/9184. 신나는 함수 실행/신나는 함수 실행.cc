ef2021aa69263f8cdd94ed15d292bb56fc6d607a
Vomeee <158576443+Vomeee@users.noreply.github.com>
2024-03-12 13:42:54 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 440 ms, Memory: 2540 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int A[51][51][51];
+
+int w(int a, int b, int c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+        return 1;
+    }
+    if (a > 20 || b > 20 || c > 20) {
+        return w(20, 20, 20);
+    }
+    if (A[a][b][c] != 0) {
+        return A[a][b][c];
+    }
+    if (a < b && b < c) {
+        A[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+    }
+    else {
+        A[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+    }
+    return A[a][b][c];
+}
+
+int main() {
+    int a, b, c;
+    while (true) {
+        cin >> a >> b >> c;
+        if (a == -1 && b == -1 && c == -1) {
+            break;
+        }
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << endl;
+    }
+    return 0;
+}
